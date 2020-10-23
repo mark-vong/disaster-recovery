9fbf851e11562b83f55a5d5a3242275b716bac2c
Mark Vong <mark.vong@oracle.com>
2020-10-23 14:09:13 -0700

    Updated folder with files

--- terraform/resource-mananger-bin/modules/iam/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dynamicGroups" {
-  value = "${data.oci_identity_dynamic_groups.dr-dynamic-groups.dynamic_groups}"
-}
-
-output "dynamicPolicies" {
-  value = "${data.oci_identity_policies.dr-dynamic-policies-1.policies}"
-}
--- terraform/resource-mananger-bin/modules/server/outputs.tf
+++
@@ -1,4 +0,0 @@
-output instance_ip {
-  description = "the private ip of the instance"
-  value       = oci_core_instance.private_server.private_ip
-}
